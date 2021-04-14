69b324905b3d5c690c4e038b765a50c3177f067e
Adam Banda <adam.banda@perficient.com>
2021-04-14 11:10:44 -0400

    Update tests

--- util/dom.ts
+++ util/dom.ts
@@ -1,6 +1,6 @@
 /*! chakra-ui v1.6.1 | MIT License | https://github.com/chakra-ui/chakra-ui/blob/4478509039b4ed9df8a7710f49b12588e1202de2/packages/utils/src/dom.ts */
 
-export function canUseDom() {
+export function canUseDom(): boolean {
   return !!(
     typeof window !== 'undefined' &&
     window.document &&
@@ -8,4 +8,4 @@ export function canUseDom() {
   )
 }
 
-export const isBrowser = canUseDom()
+export const isBrowser: boolean = canUseDom()
